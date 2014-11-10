1cc261b12e1ba65b0d3f78460b2078b016d06775
dflath <dflath@slac.stanford.edu>
2014-11-10 21:16:38 +0000

    Updated EpicsENUM columns to proper size (26 rather than 16), and EpicsString columns to proper size (40)
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pmgr@19531 ececa60f-9f63-0410-97c7-ea05888eb260

--- pmgr_db.sql
+++ pmgr_db.sql
@@ -21,34 +21,34 @@ create table ims_motor_cfg (
 	FLD_BDST  double,  -- Backlash Distance (EGU) --
 	FLD_BS    double,  -- Backlash Speed (EGU/s) --
 	FLD_DHLM  double,  -- Dial High Limit (EGU) --
-	FLD_DIR   varchar(16),  -- Direction [ENUM] --
+	FLD_DIR   varchar(26),  -- Direction [ENUM] --
 	FLD_DLLM  double,  -- Dial Low Limit (EGU) --
 	FLD_DLVL  smallint unsigned,  -- Debugging Level [DBF_SHORT] --
-	FLD_EE    varchar(16),  -- Encoder Enabled [ENUM] -- 
-	FLD_EGAG  varchar(16),  -- Use External Gauge [ENUM] --	
-	FLD_EGU   varchar(16),  -- Engineering Units Name --
+	FLD_EE    varchar(26),  -- Encoder Enabled [ENUM] -- 
+	FLD_EGAG  varchar(26),  -- Use External Gauge [ENUM] --	
+	FLD_EGU   varchar(40),  -- Engineering Units Name --
 	FLD_EL    double,  -- Encoder Lines --
-	FLD_ERBL  varchar(60),  -- External Gauge Readback Link [PV String] --
+	FLD_ERBL  varchar(40),  -- External Gauge Readback Link [PV String] --
 	FLD_ERBV  double,  -- External Guage RBV --
 	FLD_ERES  double,  -- Encoder Step Size (EGU) --
-	FLD_ERSV  varchar(16),  -- Error Severity level for reporting [ENUM] --
+	FLD_ERSV  varchar(26),  -- Error Severity level for reporting [ENUM] --
 	FLD_ESKL  double,  -- External Guage Scale --
-	FLD_FOFF  varchar(16),  -- Adjust Offset/Controller [ENUM] --
+	FLD_FOFF  varchar(26),  -- Adjust Offset/Controller [ENUM] --
 	FLD_FREV  smallint unsigned,  -- Full Steps per Rev --
 	FLD_HACC  double,  -- Homing Accel (seconds to velocity) --
 	FLD_HC    tinyint unsigned,  -- Holding current (%: 0..100) --
 	FLD_HCMX  tinyint unsigned,  -- Holding current maximum (%: 0..100) --
 	FLD_HCSV  tinyint unsigned,  -- Holding current last non-zero value (%: 0..100) --
 	FLD_HDST  double,  -- Back-off distance for limit-switch-homing (EGU) --
-	FLD_HEGE  varchar(16),  -- Homing edge of index or limit [ENUM] --
+	FLD_HEGE  varchar(26),  -- Homing edge of index or limit [ENUM] --
 	FLD_HLM   double,  -- User High Limit (EGU) --
 	FLD_HOMD  double,  -- Dial value at home (EGU) --
 	FLD_HS    double,  -- Home speed (Rev/s) --
 	FLD_HT    smallint unsigned, -- Holding Current Delay Time (ms) --
-	FLD_HTYP  varchar(16), -- Homing Type [ENUM] --
+	FLD_HTYP  varchar(26), -- Homing Type [ENUM] --
 	FLD_LLM   double,  -- User High Limit (EGU) --
-	FLD_LM    varchar(16), -- Limit Stop Mode [ENUM] --
-	FLD_MODE  varchar(16), -- Run Mode [ENUM] --
+	FLD_LM    varchar(26), -- Limit Stop Mode [ENUM] --
+	FLD_MODE  varchar(26), -- Run Mode [ENUM] --
 	FLD_MRES  double,  -- Motor Resolution (EGU/micro-step) --
 	FLD_MT    smallint unsigned,  -- Motor Settling Time (ms) --
 	FLD_OFF   double,  -- User Offset (EGU) --
@@ -60,15 +60,15 @@ create table ims_motor_cfg (
 	FLD_S     double,  -- speed (Rev/s) --
 	FLD_SBAS  double,  -- base speed (Rev/s) --
 	FLD_SF    smallint unsigned,  -- stall factor --
-	FLD_SM 	  varchar(16),  -- stall mode [ENUM] --
+	FLD_SM 	  varchar(26),  -- stall mode [ENUM] --
 	FLD_SMAX  double,  -- max speed [Rev/s] --
 	FLD_SREV  int unsigned,  -- micro-steps per revolution --
-	FLD_STSV  varchar(16),  -- stall severity level for reporting [ENUM] --
+	FLD_STSV  varchar(26),  -- stall severity level for reporting [ENUM] --
 	FLD_TWV   double,  -- Tweak value (EGU) --
-	FLD_TYPE  varchar(30) not null unique,
+	FLD_TYPE  varchar(40) not null unique,
 	FLD_UREV  double,  -- Units per Revolution (EGU/Rev) --
-	PV_FW__MEANS  varchar(16),  -- Name of forward direction --
-	PV_REV__MEANS varchar(16),  -- Name of reverse direction --
+	PV_FW__MEANS  varchar(40),  -- Name of forward direction --
+	PV_REV__MEANS varchar(40),  -- Name of reverse direction --
 
 	-- constraints --
 	primary key (id),
@@ -81,11 +81,11 @@ create table ims_motor (
 	config int not null,
 	owner varchar(10),
 	name varchar(30) not null unique,
-	rec_base varchar(30) not null,  -- pv/field base prefix --
+	rec_base varchar(40) not null,  -- pv/field base prefix --
 	dt_created datetime not null,
 	dt_updated datetime not null,
 	-- pvs and fields --
-	FLD_DESC varchar(41),  -- Description --
+	FLD_DESC varchar(40),  -- Description --
 	FLD_PORT varchar(40),  -- digi port address --	
 	
 	-- constraints --
